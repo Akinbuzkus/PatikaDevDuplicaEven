5c7743d35b37ecb67fae871da509523e1ca06ed3
Akin Buzkus <83575077+Akinbuzkus@users.noreply.github.com>
2022-12-12 18:16:19 +0300

    Add files via upload

--- PatikaDevArrayDuplicateEven.java
+++ PatikaDevArrayDuplicateEven.java
@@ -12,16 +12,16 @@ public class PatikaDevArrayDuplicateEven {
         return false;
     }
     public static void main(String[]args){
-        int [] arr={3,4,2,3,7,4,5,6,1,2,9,6,7,8,5,2,8};
-        System.out.println("Array : " + Arrays.toString(arr));
-        int duplicate []=new int[arr.length];
+        int [] list={3,4,2,3,7,4,5,6,1,2,9,6,7,8,5,2,8};
+        System.out.println("Dizi : " + Arrays.toString(list));
+        int duplicate []=new int[list.length];
         int startIndex=0;
-        for(int i=0; i<arr.length; i++){
-            for (int j=0; j<arr.length; j++){
-                if (i!=j && arr[i]==arr[j]){
-                    if (arr[i]%2==0){
-                        if (!isFind(duplicate,arr[i])){
-                            duplicate[startIndex++]=arr[i];
+        for(int i=0; i<list.length; i++){
+            for (int j=0; j<list.length; j++){
+                if (i!=j && list[i]==list[j]){
+                    if (list[i]%2==0){
+                        if (!isFind(duplicate,list[i])){
+                            duplicate[startIndex++]=list[i];
                         }
                         break;
                     }
